4b86f320b652507e53cf4e4008256bde310a1acf
Brian Holley <brian.holley@hotmail.com>
2017-03-19 21:44:29 -0700

    Hook up site functionality

--- site/routes/index.js
+++ site/routes/index.js
@@ -1,10 +1,48 @@
+var cheerio = require('cheerio');
 var express = require('express');
+var Handlebars = require('handlebars');
+var rp = require('request-promise-native')
+
 var router = express.Router();
 
-/* GET home page. */
+function parseOpenGraphMetadata(url) {
+  var og = {};
+  return rp({uri:url}).then(html => {
+    let $ = cheerio.load(html);
+    $('meta').filter((idx,m) => {
+      return m.attribs.property !== undefined && m.attribs.property.startsWith('og:');
+    }).each((idx, m) => {
+      og[m.attribs.property] = m.attribs.content;
+    });
+    return og;
+  }).catch(err => {
+    return og;
+  });
+}
+
+Handlebars.registerHelper('metadata-list', function(dict, options) {
+  var out = "";
+  if (Object.getOwnPropertyNames(dict).length !== 0) {
+    for (var key in dict) {
+      out += `<tr><td>${key}</td><td>${dict[key]}</td></tr>`;
+    }
+  } else {
+    out = "<tr><td colspan='2'>This page has no OpenGraph metadata</td></tr>";
+  }
+  return out;
+});
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  if (typeof req.query.url !== "undefined") {
+    parseOpenGraphMetadata(req.query.url).then(md => {
+      res.render('index', {
+        url: req.query.url,
+        md: md
+      });
+    });
+  } else {
+    res.render('index', {});
+  }
 });
 
 module.exports = router;
