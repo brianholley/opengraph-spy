46fd98d3ff0567c8a5347a82b23e3222327d6ec5
Brian Holley <brian.holley@hotmail.com>
2017-04-20 22:23:08 -0700

    Escape open graph data for display

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -24,7 +24,9 @@ Handlebars.registerHelper('metadata-list', function(dict, options) {
   var out = "";
   if (Object.getOwnPropertyNames(dict).length !== 0) {
     for (var key in dict) {
-      out += `<tr><td><p class="property-name">${key}</p></td><td><p class="metadata">${dict[key]}</p></td></tr>`;
+      let k = Handlebars.Utils.escapeExpression(key)
+      let v = Handlebars.Utils.escapeExpression(dict[key])
+      out += `<tr><td><p class="property-name">${k}</p></td><td><p class="metadata">${v}</p></td></tr>`;
     }
   } else {
     out = "<tr><td colspan='2'>This page has no OpenGraph metadata</td></tr>";
